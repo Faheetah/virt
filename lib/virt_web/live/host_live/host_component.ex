6b08d49e3fa6d146994ffa2c0717e1b20d6271dc
Faheetah <Faheetah@users.noreply.github.com>
2022-02-14 06:46:08 -0600

    Format hosts view

---
+++ lib/virt_web/live/host_live/host_component.ex
@@ -0,0 +1,23 @@
+defmodule VirtWeb.HostLive.HostComponent do
+  @moduledoc false
+
+  use VirtWeb, :live_component
+
+  alias Virt.Libvirt.Hosts
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:host, Virt.Repo.preload(Hosts.get_host!(id), [:pools, :domains, host_distributions: [:volume, :distribution]]))}
+  end
+
+  defp page_title(:show), do: "Show Host"
+  defp page_title(:edit), do: "Edit Host"
+end
